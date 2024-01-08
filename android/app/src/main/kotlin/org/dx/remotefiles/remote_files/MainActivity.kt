e5a9f0eb6e84f6f81404d0a1aed4153d7b49ea2a
dx <dingxing@live800.com>
2024-01-08 14:28:36 +0800

    update android remote file launch

--- android/app/src/main/kotlin/org/dx/remotefiles/remote_files/MainActivity.kt
+++ android/app/src/main/kotlin/org/dx/remotefiles/remote_files/MainActivity.kt
@@ -1,6 +1,53 @@
 package org.dx.remotefiles.remote_files
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.embedding.engine.FlutterEngine
+import android.content.Intent
+import android.net.Uri
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        val methodChannel =
+            MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "RemoteFileMethodChannel")
+        methodChannel.setMethodCallHandler { call, result ->
+            when (call.method) {
+                "launchRemoteFile" -> {
+                    val arguments: HashMap<String, Any> = call.arguments as HashMap<String, Any>
+                    val url = arguments["url"] as String?
+                    val fileType = arguments["fileType"] as String?
+                    if (url == null || fileType == null) {
+                        result.error(
+                            "launchUrl",
+                            "url or fileType is null, url=${url},fileType${fileType}",
+                            null
+                        )
+                        return@setMethodCallHandler
+                    }
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    val uri = Uri.parse(url)
+                    if (fileType == "video") {
+                        intent.setDataAndType(uri, "video/*")
+                    } else if (fileType == "audio") {
+                        intent.setDataAndType(uri, "audio/*")
+                    } else if (fileType == "compress") {
+                        intent.setDataAndType(uri, "application/*")
+                    } else if (fileType == "image") {
+                        intent.setDataAndType(uri, "image/*")
+                    } else {
+                        intent.setDataAndType(uri, "application/*")
+                    }
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+
+                    result.success(true)
+                }
+
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
 }
