b7160aeb7b8230cfd9a3a96d01e033ba6f960cad
dx <dingxing@live800.com>
2024-08-06 16:28:16 +0800

    support web

--- lib/method_channel/remote_file_method_channel.dart
+++ lib/method_channel/remote_file_method_channel.dart
@@ -1,6 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/services.dart';
+import 'package:remote_files/app.dart';
 import 'package:remote_files/entities/remote_file.dart';
 import 'package:remote_files/utils/file_utils.dart';
 
@@ -8,7 +7,7 @@ class RemoteFileMethodChannel {
   static const MethodChannel _channel = MethodChannel("RemoteFileMethodChannel");
 
   static void launchRemoteFile(RemoteFile remoteFile) {
-    if (Platform.isAndroid) {
+    if (App.isAndroid) {
       FileType fileType = FileUtils.getFileType(remoteFile.fileName);
       _channel.invokeMethod("launchRemoteFile", {"fileType": fileType.name, "url": remoteFile.url});
     }
